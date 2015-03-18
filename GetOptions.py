19ba3f75a8370200d81be103a4de8f47419b156e
Pugh, Matthew Owen <mopugh@sandia.gov>
2015-03-17 19:36:18 -0700

    modified GetOptions.py to correct method name

--- GetOptions.py
+++ GetOptions.py
@@ -6,8 +6,8 @@ datadir = 'DataFiles/'
 read_filename = datadir + 'SP500.csv'
 SP500_names = pd.read_csv(read_filename)
 # Change filename to date
-write_filename = '03162015.csv'
+write_filename = '03172015.csv'
 write_filename = datadir + write_filename
 
-TotalOptionsChain = StockInfo.GetSP500Option(read_filename)
+TotalOptionsChain = StockInfo.GetSP500Options(read_filename)
 TotalOptionsChain.to_csv(write_filename)
